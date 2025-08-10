576c9106183adbf0d68cfc07475978cf0201a38d
AkashSH0007 <akash.eai24@chanakyauniversity.edu.in>
2025-08-10 17:27:30 +0530

    Add files via upload

---
+++ student marks grader.py
@@ -0,0 +1,33 @@
+def grade(percentage):   
+    if percentage >= 90:
+        return "A"
+    elif percentage >= 80:
+        return "B"
+    elif percentage >= 70:
+        return "C"
+    elif percentage >= 60:
+        return "D"
+    else:
+        return "F"
+
+name = input("Enter student name: ")
+
+num_subjects = int(input("Enter number of subjects: "))
+Marks=int(input("Enter max marks of subject"))
+
+total_marks = 0
+total_max = num_subjects * Marks  
+
+for i in range(num_subjects):           # using loop for subjects marks
+    marks = float(input(f"Enter marks for Subject {i+1}: "))
+    total_marks += marks
+
+percentage = (total_marks / total_max) * 100  #for percentage calc
+grade =grade(percentage)
+
+print("\n----- Student Report -----")
+print(f"Name       : {name}")
+print(f"Total Marks: {total_marks}/{total_max}")
+print(f"Percentage : {percentage:.2f}%")
+print(f"Grade      : {grade}")
+print("--------------------------")
